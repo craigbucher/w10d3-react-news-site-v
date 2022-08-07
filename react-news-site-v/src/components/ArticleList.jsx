39119926d7a43c0ce187ef7ea7742a03e5df4639
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-08-07 16:44:13 -0500

    initial push

---
+++ react-news-site-v/src/components/ArticleList.jsx
@@ -0,0 +1,16 @@
+import ArticleTeaser from "./ArticleTeaser"
+
+function ArticleList({ articles }) {
+
+
+    return (
+        <div>
+            {articles.map((article) => (
+                <ArticleTeaser key={article.objectID} {...article} />
+            ))
+            }
+        </div>
+    )
+}
+
+export default ArticleList
